d76b27cd94a6b7a63e94a2e8196e86f20ae00e05
Andreas Neumann <andy@neumann-home.net>
2025-12-28 19:41:27 +0100

    improve dev experience

--- abi-test.ts
+++ abi-test.ts
@@ -3,12 +3,17 @@ import erc20Abi from "./erc20.json";
 const config = {
     contracts: [
         {
-            name: "BAYCxx",
+            name: "WETH",
             abi: erc20Abi,
-            address: "0x1ef840cfaaf7225f37388fcd1669ae7e17072ef4",
+            address: "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9",
+        },
+        {
+            name: "LINK",
+            abi: erc20Abi,
+            address: "0x779877A7B0D9E8603169DdbD7836e478b4624789",
         },
     ],
-    chainId: 1,
+    chainId: 11155111,
 };
 
 export default config;
